31ad8e454f8aa3ceffc231785df688578480c17b
flysherdev11 <1>
2019-06-13 08:04:32 +0900

    12

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #javascriptTest
+z
--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-(function(){
-    console.log("hello javascript");
-})();
-
+// (function(){
+//     console.log("hello javascript");
+// })();
+//
 
 
 // 전역 네임 스페이스는 모두 대문자 혹은 대문자로 시작
+
+
+
+
+var test = function() {
+
+    var testValue01 = 1;
+    var testValue02 = 2;
+
+    this.testtestValue01 = 3;
+
+    var testFunc = function () {
+        console.log("testfunc");
+    };
+
+    return {
+        testV1 : testValue01,
+        testV2 : testValue02,
+        testFunc : testFunc();
+    }
+
+};
+
+var testObj = new test();
+
+console.log( testObj.testV1 );
+console.log( testObj.testValue01 );
+console.log( testObj.testtestValue01 );
+testObj.testValue01 = 10;
+console.log( testObj.testValue01 );
